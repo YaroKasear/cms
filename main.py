5bbafc9c5130d9e309ba05ba9a4122a13ce69dfd
Yaro Kasear <yarokasear@gmail.com>
2023-09-24 23:34:37 -0500

    Some slow progress on getting the database working.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
+from sqlmodel import Session, select
+
+from models import *
+from database import *
+
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    database.create_db_and_tables()
+    with Session(database.engine) as session:
+        statement = select(models.Hero)
+
 
 @app.get('/')
 async def root():
--- models.py
+++ models.py
@@ -6,84 +6,36 @@ from sqlmodel import Field, Relationship, SQLModel
 
 
 class ContentType(Enum):
-    TEXT = "text"
-    COMMENT = "comment"
-    IMAGE = "image"
-    AUDIO = "audio"
-    VIDEO = "video"
+    TEXT = 0
+    COMMENT = 1
+    IMAGE = 2
+    AUDIO = 3
+    VIDEO = 4
 
 
 class PermissionType(Enum):
-    CREATE = "create"
-    READ = "read"
-    UPDATE = "update"
-    DELETE = "delete"
-    MODERATE = "moderate"
-    ADMIN = "admin"
-    OWNER = "owner"
+    CREATE = 0
+    READ = 1
+    UPDATE = 2
+    DELETE = 3
+    MODERATE = 4
+    ADMIN = 5
+    OWNER = 6
 
 
 class PermissionRole(Enum):
-    GUEST = "guest"
-    USER = "user"
-    AUTHOR = "author"
-
-
-class Content(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    type: ContentType
-    filepath: str = Field(unique=True)
-    owner_id: int = Field(foreign_key="user.id")
-
-    owner: "User" = Relationship(back_populates="content")
-    tags: List["Tag"] = Relationship(back_populates="contents", link_model="ContentTag")
-
-
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(unique=True)
-    email: str = Field(unique=True)
-
-    content: List[Content] = Relationship(back_populates="owner")
-    groups: List["Group"] = Relationship(back_populates="users", link_model="UserGroup")
-    permissions: List["Permission"] = Relationship(
-        back_populates="users", link_model="UserPermission"
-    )
-
-
-class Group(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(unique=True)
-
-    users: List[User] = Relationship(back_populates="groups", link_model="UserGroup")
-
-
-class Permission(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    type: PermissionType
-    role: PermissionRole
-
-    users: List[User] = Relationship(
-        back_populates="permissions", link_model="UserPermission"
-    )
-    groups: List[Group] = Relationship(
-        back_populates="permissions", link_model="GroupPermission"
-    )
-
-
-class Tag(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(unique=True)
-
-    contants: List[Content] = Relationship(
-        back_populates="tags", link_model="ContentTag"
-    )
+    GUEST = 0
+    USER = 1
+    AUTHOR = 2
 
 
 class UserGroup(SQLModel, table=True):
-    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
-    group_id: Optional[int] = Field(default=None, foreign_key="group.id")
+    user_id: Optional[int] = Field(
+        default=None, foreign_key="user.id", primary_key=True
+    )
+    group_id: Optional[int] = Field(
+        default=None, foreign_key="group.id", primary_key=True
+    )
 
 
 class UserPermission(SQLModel, table=True):
@@ -109,3 +61,56 @@ class ContentTag(SQLModel, table=True):
         default=None, foreign_key="content.id", primary_key=True
     )
     tag_id: Optional[int] = Field(default=None, foreign_key="tag.id", primary_key=True)
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(unique=True)
+    email: str = Field(unique=True)
+
+    content: List["Content"] = Relationship(back_populates="owner")
+    groups: List["Group"] = Relationship(back_populates="users", link_model=UserGroup)
+    permissions: List["Permission"] = Relationship(
+        back_populates="users", link_model=UserPermission
+    )
+
+
+class Content(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    type: ContentType
+    filepath: str = Field(unique=True)
+    owner_id: int = Field(foreign_key="user.id")
+
+    owner: User = Relationship(back_populates="content")
+    tags: List["Tag"] = Relationship(back_populates="contents", link_model=ContentTag)
+
+
+class Group(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(unique=True)
+
+    users: List[User] = Relationship(back_populates="groups", link_model=UserGroup)
+    permissions: List["Permission"] = Relationship(
+        back_populates="groups", link_model=GroupPermission
+    )
+
+
+class Permission(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    type: PermissionType
+    role: PermissionRole
+
+    users: List[User] = Relationship(
+        back_populates="permissions", link_model=UserPermission
+    )
+    groups: List[Group] = Relationship(
+        back_populates="permissions", link_model=GroupPermission
+    )
+
+
+class Tag(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(unique=True)
+
+    contents: List[Content] = Relationship(back_populates="tags", link_model=ContentTag)
